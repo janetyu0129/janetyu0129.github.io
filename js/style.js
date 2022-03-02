8ce83436f50ad87986aae8dd2175435d374a5c69
Janet Yu <>
2022-03-02 10:28:29 +0800

    0302 update

--- js/style.js
+++ js/style.js
@@ -1,16 +1,54 @@
 $(document).ready(function() {
+    //Counting Numbers
+    $(".counter").each(function() {
+        var $this = $(this),
+            countTo = $this.attr("data-count");
+        $({ countNum: $this.text() }).animate({
+            countNum: countTo
+        }, {
+            duration: 1000,
+            easing: "linear",
+            step: function() {
+                $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+                $this.text(this.countNum);
+            }
+        });
+    });
+
     //hover調整公司邊框顏色
     var initial = $(".cp-work").css("border-color");
     $(".cp-work").hover(
         function() {
             var color = $(this).attr("value");
-            $(this).css("box-shadow", "0 4px 0 " + color).css("border-color", color);
+            $(this).css("border-color", color);
+            // $(this).css("box-shadow", "0 4px 0 " + color);
         },
         function() {
-            $(this).css("box-shadow", "0 4px 0 " + initial).css("border-color", initial);
+            $(this).css("border-color", initial);
+            // $(this).css("box-shadow", "0 4px 0 " + initial);
         }
     );
+
+    //漢堡下拉選單展開時加背景色
     $(".navbar-toggler").click(function() {
         $(".bg-light").toggleClass("custom-navbar-bg");
-    })
+    });
+
+    //Slick CSS
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }]
+    });
 });
